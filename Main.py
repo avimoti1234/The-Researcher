ee8e357aa47eeb9dbb37826188f75369b396aba6
omer lachover <omerielachover@gmail.com>
2021-12-12 11:17:53 -0500

    1+1=2

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
 import os
-
 if os.name != "posix" or os.getuid():
-    print(
-        "[!]Your system does not meet the standard requirements for this tool to run.\n[*]Try to to run this on unix based system with sudo.")
+    print("[!]Your system does not meet the standard requirements for this tool to run.\n[*]Try to to run this on unix based system with sudo.")
     exit(1)
 from src import AndroidControlTool, MlwareAndNetcat, NetworkingStuff
 
