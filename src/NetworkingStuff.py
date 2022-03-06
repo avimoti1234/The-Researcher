47f7dbb041a6ad6990e8916fbbf7755e3b4e263a
avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
2022-03-06 20:49:01 +0200

    updated somethigs

--- src/NetworkingStuff.py
+++ src/NetworkingStuff.py
@@ -2,12 +2,26 @@ from scapy.all import ICMP, ARP, IP, UDP, TCP, Ether, RandShort, RandIP, send
 from threading import Thread
 import socket
 
+def DisplayHelpMenu():
+    HelpMenu = \
+    """
+    researcher [option] [parameter]
+    options:
+    
+    
+    -p --protocol\tspecify protocol to use
+    -f --flags   \tspecify flags in the packet header of the protocol you chose
+    """
+    print(HelpMenu)
+
 class PortScanner:
     def __init__(self):
         pass
 
 class Ddos:
     def __init__(self):
+        self.Join = True
+        self.Attack = False
         mode = input("\n   [*]choose mode(server or client): ")
         if mode == "server":
            self._ServerMode()
@@ -16,13 +30,15 @@ class Ddos:
      
     def _ServerMode(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        Ip = input("\n   [*]ip to be bound: ")
+        Ip = input("\n   [*]Ip to be bound: ")
 
-        Port = int(input("\n   [*]port to listen on: "))
+        self.TargetIp = input("\n   [*]Enter target ip: ")
+
+        Port = int(input("\n   [*]Port to listen on: "))
 
         self.sock.bind((Ip, Port))
 
-        NumOfPeople = int(input("\n   [*]max number of members to join: "))
+        NumOfPeople = int(input("\n   [*]Max number of members to join: "))
 
         self.sock.listen(NumOfPeople)
 
@@ -31,23 +47,32 @@ class Ddos:
 
         Input = ""
         while Input != "exit":
-            Input = input("   [*]host@command>")
-            if Input == "attack":
-                #create attack fuction
-                pass
-            elif Input == "stop":
-                #set variable to False thus the loop of the ddos will stop
-                pass
+            Input = input("   host@command>")
+            if Input == "start attack":
+                self.Attack = True
+                self.AttackTarget()
+            elif Input == "stop attack":
+                self.Attack = False
+            elif Input == "stop join":
+                self.Join = False
+            elif Input == "-h" or Input == "--help":
+                DisplayHelpMenu()
             else:
                 for i in range(len(self.members) - 1):
                     self.members[i].send(Input.encode())
+        self.Join = False
+        print(122)
 
-        #self.sock.close()
+
+    def AttackTarget(self):
+        while self.Attack:
+            send()
 
     def _JoinParty(self):
         self.members = []
-        while True:
+        while self.Join:
             client, addr = self.sock.accept()
             self.members.append(client)
+            print(self.Join)
 
-
+        print(12)
