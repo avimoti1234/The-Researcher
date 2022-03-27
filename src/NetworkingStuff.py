832d559f64eab17f9d2474c2ad4e4fe38deba25a
avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
2022-03-27 22:48:24 +0300

    added some things and fixed a couple of things

--- src/NetworkingStuff.py
+++ src/NetworkingStuff.py
@@ -2,21 +2,27 @@ from scapy.all import ICMP, ARP, IP, UDP, TCP, Ether, RandShort, RandIP, send
 from threading import Thread
 import socket
 
+
 def DisplayHelpMenu():
     HelpMenu = \
-    """
+        """
     researcher [option] [parameter]
     options:
     
     
-    -p --protocol\tspecify protocol to use
-    -f --flags   \tspecify flags in the packet header of the protocol you chose
+    -p --protocol      \tspecify protocol to use
+    -f --flags         \tspecify flags in the packet header of the protocol you chose
+    -port              \tspecify port number for the attack
+        1. -port/(nothing)       \tif -port set to nothing the port will be set to default
+        2. -port random\tif -port set to random the attack will use random port to attack
     """
     print(HelpMenu)
+
 
 class PortScanner:
     def __init__(self):
         pass
+
 
 class Ddos:
     def __init__(self):
@@ -24,10 +30,10 @@ class Ddos:
         self.Attack = False
         mode = input("\n   [*]choose mode(server or client): ")
         if mode == "server":
-           self._ServerMode()
+            self._ServerMode()
         elif mode == "client":
             self.ClientMode()
-     
+
     def _ServerMode(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         Ip = input("\n   [*]Ip to be bound: ")
@@ -46,8 +52,8 @@ class Ddos:
         thrd.start()
 
         Input = ""
-        while Input != "exit":
-            Input = input("   host@command>")
+        while Input != "exit\n":
+            Input = input("   host@command>") + '\n'
             if Input == "start attack":
                 self.Attack = True
                 self.AttackTarget()
@@ -55,6 +61,8 @@ class Ddos:
                 self.Attack = False
             elif Input == "stop join":
                 self.Join = False
+            elif Input == "resume join":
+                self.Join = True
             elif Input == "-h" or Input == "--help":
                 DisplayHelpMenu()
             else:
