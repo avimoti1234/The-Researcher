5224c9f7778d69f01890996291b5eb809ece7af5
avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
2022-07-14 14:56:49 +0300

    Update MlwareAndNetcat.py

--- src/MlwareAndNetcat.py
+++ src/MlwareAndNetcat.py
@@ -1,3 +1,21 @@
 from threading import Thread
 import socket
 import subprocess
+
+
+class NetCat:
+    def __init__(self, mode: str):
+        if mode == "server":
+            self.ServerMode()
+        elif mode == "client":
+            self.ClientMode()
+        else:
+            print("[!] The following input is invalid mode.\nplease choose valid mode(server/client).")
+
+    def ServerMode(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+
+
