a07d57ba2a0cc07320057defdd5452edc6080d97
avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
2022-07-15 01:14:35 +0300

    Update MlwareAndNetcat.py

--- src/MlwareAndNetcat.py
+++ src/MlwareAndNetcat.py
@@ -10,12 +10,20 @@ class NetCat:
         elif mode == "client":
             self.ClientMode()
         else:
-            print("[!] The following input is invalid mode.\nplease choose valid mode(server/client).")
+            print("   [!]The following input is invalid mode.\nplease choose valid mode(server/client).")
+
 
     def ServerMode(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.IP = input("   [*]What ip do you want to listen on? ")
+        self.PortNumber = int(input("   [*]What port do you to listen on? "))
 
+        self.sock.bind((self.IP, self.PortNumber))
 
+        NumOfPeopleToJoin = int(input("   [*]How many people would you like to join? "))
+
+        self.sock.listen(NumOfPeopleToJoin)
+         
 
 
 
