43737961b481ec1e2c080540c3f05d70958c038e
avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
2022-07-16 16:23:09 +0300

    Update MlwareAndNetcat.py

--- src/MlwareAndNetcat.py
+++ src/MlwareAndNetcat.py
@@ -12,7 +12,6 @@ class NetCat:
         else:
             print("   [!]The following input is invalid mode.\nplease choose valid mode(server/client).")
 
-
     def ServerMode(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.IP = input("   [*]What ip do you want to listen on? ")
@@ -23,10 +22,8 @@ class NetCat:
 
         self.sock.listen(1)
         print("   [*]Socket has been initiated, looking for incoming connection...\n")
-        sock_handler, niglet= self.sock.accept()
+        sock_handler, IP = self.sock.accept()
 
-        print(niglet)
-        sock_handler.close()
-        self.sock.close()
-        exit(1)
+
+
 
