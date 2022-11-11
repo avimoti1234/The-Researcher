3e7825012581a6d0800aa0abdf17a701a6505851
avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
2022-11-11 11:14:00 +0200

    Update MlwareAndNetcat.py

--- src/MlwareAndNetcat.py
+++ src/MlwareAndNetcat.py
@@ -14,15 +14,24 @@ class NetCat:
 
     def ServerMode(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.IP = input("   [*]What ip do you want to listen on? ")
+        Ip = input("   [*]What ip do you want to listen on? ")
         self.PortNumber = int(input("   [*]What port do you to listen on? "))
 
-        self.sock.bind((self.IP, self.PortNumber))
+        self.sock.bind((Ip, self.PortNumber))
         print("   [*]Socket has been bound successfully")
 
         self.sock.listen(1)
         print("   [*]Socket has been initiated, looking for incoming connection...\n")
         sock_handler, IP = self.sock.accept()
+
+
+    def ClientMode(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        Ip = input("   [*]Enter ip to connect: ")
+        self.sock.connect((Ip, self.PortNumber))
+        print("   [*]Socket has been initialized and connected to end point host.")
+
 
 
 
